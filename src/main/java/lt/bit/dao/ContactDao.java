2bc337082e7c9bb0c9c6a1459f4ed46a69fa003a
ZilvinasLasauskas <51168294+ZilvinasLasauskas@users.noreply.github.com>
2019-11-11 17:32:38 +0200

    Delete ContactDao.java

--- src/main/java/lt/bit/dao/ContactDao.java
+++
@@ -1,8 +0,0 @@
-package lt.bit.dao;
-
-import lt.bit.data.Contact;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContactDao extends JpaRepository<Contact, Integer> {
-    
-}
