7abf9c60a7cfcdb00ecdef02b6a79fb862643cb6
ZilvinasLasauskas <51168294+ZilvinasLasauskas@users.noreply.github.com>
2019-11-11 17:15:53 +0200

    Add files via upload

---
+++ src/main/java/lt/bit/dao/ContactDao.java
@@ -0,0 +1,8 @@
+package lt.bit.dao;
+
+import lt.bit.data.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContactDao extends JpaRepository<Contact, Integer> {
+    
+}
