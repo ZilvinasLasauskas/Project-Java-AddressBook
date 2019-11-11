8ab7903868559ddcbaa49c4110a27829c81ed566
ZilvinasLasauskas <51168294+ZilvinasLasauskas@users.noreply.github.com>
2019-11-11 17:32:28 +0200

    Delete AddressDao.java

--- src/main/java/lt/bit/dao/AddressDao.java
+++
@@ -1,8 +0,0 @@
-package lt.bit.dao;
-
-import lt.bit.data.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AddressDao extends JpaRepository<Address, Integer> {
- 
-}
